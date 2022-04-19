b36fcc169409977af1340afaf7d09ceaf0d539e0
mspar <marc.sparhuber@gmail.com>
2022-04-19 15:48:31 +0200

    New viz with halfed/symmetrical violin+dots

--- Test_Skript_Marc.R
+++ Test_Skript_Marc.R
@@ -5,6 +5,9 @@ library(ggdist)
 library(hrbrthemes)
 library(bain)
 library(grid)
+library(ggdist)
+library(ggridges)
+library(ggbeeswarm)
 
 # Vector of sample sizes
 mean_group_A <- 50
@@ -70,6 +73,67 @@ ggplot(data %>%
   ylab("Value")
   
 # Add another title outside y-axis (right)
+
+
+## DATA VIS WITH ggdist
+
+#Ausprobieren:
+#Titel in jedem Strip Wiederholen (?)
+#Welche Vis ist konsistent/geeignet zur Verbalisierung “Overlap = 90%”
+#Ausprobieren: quasirandom mit Violine im Hintergrund (symmetrisch und halbiert) (ggdist package)
+
+
+ggplot(data %>% 
+         gather(Group, value, A, B) %>% 
+         mutate(Overlap = as.factor(Overlap),
+                `Group Size` = as.factor(`Sample Size`)), 
+       aes(Group, value)) +
+  
+  #symmetrisch
+  #stat_slab(side = "both", scale = 0.3, position = "dodgejust") +
+  #geom_violin() +
+  #geom_quasirandom(width = 0.2) +
+  
+  #possibly get geom_violinhalf?
+  
+  #halbiert
+  stat_slab(side = "left", scale = 0.3, fill = "black") +
+  stat_dots(side = "right", scale = 0.25, quantiles = 50, fill = "black") +
+  
+  
+  stat_summary(
+    fun = mean, geom = "point", 
+    shape = 95, size = 10, color = "orange"
+  ) +
+  facet_wrap(Overlap ~ `Group Size`,
+             labeller = labeller(`Group Size` = as_labeller(c(`10` = "Group size = 10",
+                                                              `20` = "Group size = 20",
+                                                              `40` = "Group size = 40",
+                                                              `80` = "Group size = 80")),
+                                 Overlap = as_labeller(c(`0.69` = "Overlap between groups A and B = 69%",
+                                                         `0.8` = "Overlap between groups A and B = 80%",
+                                                         `0.92` = "Overlap between groups A and B = 92%",
+                                                         `0.96` = "Overlap between groups A and B = 96%"))),
+             scales = "free") +
+  ylab("Value") +
+  theme(panel.spacing = unit(0.35, "cm"),
+        panel.grid.major.y = element_line(size = 0.5, linetype = 'solid', colour = "grey"),
+        panel.grid.minor.y = element_line(size = 0.5, linetype = 'solid', colour = "grey"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        strip.background = element_rect(color="black", fill="#5ebc3b", size=1.5, linetype="solid"),
+        panel.background = element_rect(fill = "white", colour = "black"),
+        strip.text.x = element_text(size = 12, color = "black", face = "bold"),
+        strip.text.y = element_text(size = 12, color = "black", face = "bold", angle = 360),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank()
+  ) +
+  ylim(7,83)
+
+#es ist möglich, die Gruppen so anzuordnen, wie in facet_grid, ist aber ein bisschen umständlich:
+#https://stackoverflow.com/questions/52706599/how-to-position-strip-labels-in-facet-wrap-like-in-facet-grid
+#try to move the groups closer to one another
+
 
 # Single graph
 
