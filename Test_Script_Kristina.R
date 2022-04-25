14d61bf62cc7edf5df2e34b125fadfed0a7f3af9
KristinaBohrer <kristina.bohrer@ph-karlsruhe.de>
2022-04-25 16:39:42 +0200

    try and ERROR mean difference label

--- Test_Script_Kristina.R
+++ Test_Script_Kristina.R
@@ -7,6 +7,7 @@ library(hrbrthemes)
 library(bain)
 library(BayesFactor)
 library(lemon)
+library(ggpubr)
 
 # Vector of sample sizes
 mean_group_A <- 50
@@ -44,7 +45,7 @@ for(i in sample_sizes){
 }
 
 
-# Visualize the data - dotplot
+## Visualize the data - dotplot
 ggplot(data %>% 
          gather(Group, value, A, B) %>% 
          mutate(Overlap = as.factor(Overlap),
@@ -62,9 +63,13 @@ ggplot(data %>%
 
 # To-do:
 # - groups closer together -> to show the overlap
-# - 
+# - Matrix Charakter durch Farbe des Strips hervorheben
+# - Tendenz: Entweder
+#   - Nur Points mit Mittelwertsdifferenzlabel oder
+#   - Points auf Violine mit Overlaplabel
 
-# Visualize the data - beeswarm + violin plot groups on x axis
+
+## Visualize the data - beeswarm + violin plot groups on x axis
 ggplot(data %>% 
          gather(Group, value, A, B) %>% 
          mutate(Overlap = as.factor(Overlap),
@@ -83,8 +88,40 @@ ggplot(data %>%
                  labeller = label_both) + 
   theme_ipsum_rc() 
 
-# Visualize the data - beeswarm + violin plot groups on x axis, Points auf Violine mit Overlaplabel
+
+## Visualize the data - beeswarm + violin plot groups on y axis
+ggplot(data %>% 
+         gather(Group, value, A, B) %>% 
+         mutate(Overlap = as.factor(Overlap),
+                `Sample Size` = as.factor(`Sample Size`)), 
+       aes(value, Group)) + 
+  geom_violin() +
+  geom_quasirandom(colour = "#848484",
+                   groupOnX = F) + 
+  stat_summary(
+    fun = mean, geom = "point", 
+    shape = 95, size = 10, color = "#8cd000"
+  ) + 
+  facet_rep_grid(Overlap ~ `Sample Size`,
+                 repeat.tick.labels = T,
+                 labeller = label_both) + 
+  theme_ipsum_rc() 
+  
+## Inspect the evidence 
+data %>% 
+  gather(Group, value, A, B) %>% 
+  nest_by(`Sample Size`, cohen_d) %>% 
+  mutate(PmP = bain(t_test(value ~ Group, data = data), 
+                    hypothesis = "groupA > groupB")$fit$PMPc[1],
+         AABF = bain(t_test(value ~ Group, data = data), 
+                   hypothesis = "groupA > groupB")$fit$BF[1],
+         JZSBF = extractBF(ttestBF(formula = value ~ Group, data = data))$bf,
+         pval = t.test(value ~ Group, data = data)$p.value)
+
+
+## Visualize the data - beeswarm + violin plot groups on x axis, Points auf Violine mit Overlaplabel
 # to add: Matrix Charakter durch Farbe der Strips hervorheben, ggf. anderes Design dafür wählen
+
 ggplot(data %>% 
          gather(Group, value, A, B) %>% 
          mutate(Overlap = as.factor(Overlap),
@@ -111,37 +148,57 @@ ggplot(data %>%
   ylab("Value") + 
   theme_ipsum_rc() 
 
-# Visualize the data - beeswarm + violin plot groups on y axis
+## Visualize the data - beeswarm + violin plot groups on x axis, nur Points mit Mittelwertsdifferenzlabel
+# to add: Matrix Charakter durch Farbe der Strips hervorheben, ggf. anderes Design dafür wählen
+
+data_long <- data %>%
+  pivot_longer(cols = A:B,
+               names_to = "Group",
+               values_to = "Value"
+                 ) 
+
+#integrierter p-Wert des Mittelwertsunterschieds... -> brauchen wir nicht
+ggplot(data_long,
+       aes(Group, Value)) +
+  geom_quasirandom() +
+  stat_compare_means()
+
+#neuer Ansatz:
+#1. Mittelwertsdifferenzen berechnen
+#2. in neuer Variable anlegen
+#3. Variable als factor umbenennenn
+#4. Überschriften als Kategorie des factors
+#5. die Variable in Plot einbauen - danach facet_wrap aufteilen
+#dann aber 
+
+
 ggplot(data %>% 
          gather(Group, value, A, B) %>% 
          mutate(Overlap = as.factor(Overlap),
-                `Sample Size` = as.factor(`Sample Size`)), 
-       aes(value, Group)) + 
-  geom_violin() +
-  geom_quasirandom(colour = "#848484",
-                   groupOnX = F) + 
+                `Group Size` = as.factor(`Sample Size`)), 
+       aes(Group, value)) + 
+  geom_quasirandom(                #draws jittered data points similarly to geom_jitter but reducing overplotting
+    colour = "#848484",
+    cex = 2) + 
   stat_summary(
     fun = mean, geom = "point", 
     shape = 95, size = 10, color = "#8cd000"
   ) + 
-  facet_rep_grid(Overlap ~ `Sample Size`,
-                 repeat.tick.labels = T,
-                 labeller = label_both) + 
+  facet_wrap(Overlap ~ `Group Size`,
+             labeller = labeller(`Group Size` = as_labeller(c(`10` = "Group size = 10",
+                                                              `20` = "Group size = 20",
+                                                              `40` = "Group size = 40",
+                                                              `80` = "Group size = 80")),
+                                 Overlap = as_labeller(c(`0.69` = "Overlap between groups A and B = 69%",
+                                                         `0.8` = "Overlap between groups A and B = 80%",
+                                                         `0.92` = "Overlap between groups A and B = 92%",
+                                                         `0.96` = "Overlap between groups A and B = 96%"))),
+             scales = "free") +
+  ylab("Value") +
   theme_ipsum_rc() 
-  
-# Inspect the evidence 
-data %>% 
-  gather(Group, value, A, B) %>% 
-  nest_by(`Sample Size`, cohen_d) %>% 
-  mutate(PmP = bain(t_test(value ~ Group, data = data), 
-                    hypothesis = "groupA > groupB")$fit$PMPc[1],
-         AABF = bain(t_test(value ~ Group, data = data), 
-                   hypothesis = "groupA > groupB")$fit$BF[1],
-         JZSBF = extractBF(ttestBF(formula = value ~ Group, data = data))$bf,
-         pval = t.test(value ~ Group, data = data)$p.value)
 
 
-# Save a bunch of plots programmatically
+## Save a bunch of plots programmatically
 for(j in effect_sizes){
   temporary_data <- 
     tibble(
